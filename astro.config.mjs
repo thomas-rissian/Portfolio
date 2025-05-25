5529fa10e328a7d21e8aca54da40e3fe24fc4322
thrissian <thomas.rissian@gmail.com>
2025-05-25 20:50:19 +0200

    fix: set base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
-import { defineConfig } from 'astro/config';
-
 export default defineConfig({
   output: 'static',
   base: '/Portfolio/', // ⚠️ change-le !
-});
+}); // ← ajoute un commentaire ici temporairement
